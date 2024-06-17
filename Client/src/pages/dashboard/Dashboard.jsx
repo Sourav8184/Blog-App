1c3f36a66bcea42a50c3407cd70435595244be67
Sourav <ksourav662@gmail.com>
2024-06-17 17:27:37 +0530

    Add dashBoard Component

--- Client/src/pages/dashboard/Dashboard.jsx
+++ Client/src/pages/dashboard/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from "../../components/dashprofile/DashProfile";
 import DashPosts from "../../components/dashPosts/DashPosts";
 import DashUsers from "../../components/dashUsers/DashUsers";
 import DashComment from "../../components/dashComment.jsx/DashComment";
+import DashBoardComp from "../../components/dashBoard/DashBoardComp";
 
 function Dashboard() {
   const location = useLocation();
@@ -27,6 +28,7 @@ function Dashboard() {
       {tab === "posts" && <DashPosts />}
       {tab === "users" && <DashUsers />}
       {tab === "comments" && <DashComment />}
+      {tab === "dash" && <DashBoardComp />}
     </div>
   );
 }
